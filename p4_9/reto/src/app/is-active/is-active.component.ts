d1348d58ce255103ab45c108fedf83f1923bb760
marwingaray <marwingaray@gmail.com>
2023-01-10 17:32:20 -0500

    Session 9 reto

---
+++ p4_9/reto/src/app/is-active/is-active.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-is-active',
+  templateUrl: './is-active.component.html',
+  styleUrls: ['./is-active.component.css']
+})
+export class IsActiveComponent {
+  check = false;
+}
